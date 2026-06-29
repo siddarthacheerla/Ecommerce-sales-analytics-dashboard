3fd2668bb5b81e43c03fa1fa83be885b0751286a
Cheerla Siddartha <siddarthacheerla@gmail.com>
2026-06-29 19:58:28 +0530

    Add files via upload

---
+++ ecommerce_analytics.sql
@@ -0,0 +1,14 @@
+create database ecommerce_analytics;
+use ecommerce_analytics;
+
+show tables;
+
+SELECT COUNT(*) FROM olist_customers_dataset;
+SELECT COUNT(*) FROM olist_order_items_dataset;
+SELECT COUNT(*) FROM olist_order_payments_dataset;
+SELECT COUNT(*) FROM olist_orders_dataset;
+SELECT COUNT(*) FROM olist_products_dataset;
+SELECT COUNT(*) FROM olist_sellers_dataset;
+
+
+
